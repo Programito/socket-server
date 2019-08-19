a26ff6f9fb6e71b347cebf8d76348cf0d6574d9f
Programito <Programito@gmail.com>
2019-08-19 17:19:30 +0200

    enviar sockets

--- classes/server.ts
+++ classes/server.ts
@@ -1,20 +1,63 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
+
+import * as socket from '../sockets/sockets';
 
 export default class Server{
+    
+    //Singleton server
+    private static _instance:Server;
+
 
     public app: express.Application;
     public port:number;
 
-    constructor() {
+    public io:socketIO.Server;
+
+    private httpServer:http.Server;
+
+
+    private constructor() {
 
         this.app = express();
         this.port= SERVER_PORT;
 
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+
+        this.escucharSockets();
     }
 
-    start(callback: Function){
-        this.app.listen(this.port, callback);
+    public static get instance(){
+        return this._instance || (this._instance = new this());
+    }
+
+    private escucharSockets(){
+        console.log('Escuchando conexiones - sockets');
+
+        this.io.on('connect', cliente => {
+
+            console.log("Cliente conectado");
+
+            // Mensajes
+            socket.mensaje(cliente, this.io);
+        
+            //cliente.on('disconnect', () => {
+                //console.log('Cliente Desconectado');
+            //});
+            // Desconectar
+            socket.desconnectar(cliente);
+
+        });
+    }
+
+    start( callback: Function ) {
+
+        //this.app.listen( this.port, callback );
+        this.httpServer.listen( this.port, callback );
+
     }
 
 }
